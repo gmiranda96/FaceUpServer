16c4c82486914029fd94c9fe546f6454d1ba8e5a
GtheCoder96 <gabemiranda209@gmail.com>
2022-12-22 17:40:22 -0400

    adding port

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
-app.listen(3000, () => {
-	console.log('app is running');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 })
