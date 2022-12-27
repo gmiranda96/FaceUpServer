244895537080b22144f2650a5545b0b66e984521
GtheCoder96 <gabemiranda209@gmail.com>
2022-12-27 18:23:58 -0400

    adding db connection

--- server.js
+++ server.js
@@ -12,11 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-infinite-72722',
-    port : 5432,
-    user : 'postgres',
-    password : '987412365G',
-    database : 'faceupdb'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
